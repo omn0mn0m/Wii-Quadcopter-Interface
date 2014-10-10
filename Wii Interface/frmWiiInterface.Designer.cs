73189193bf2fb781b790bead2453a80100474ef5
Nam Tran <tranngocnam97@yahoo.com>
2014-10-10 01:36:55 -0400

    Initial Commit of Code
    - Working interface that displays values from the Wii remote

---
+++ Wii Interface/frmWiiInterface.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace Wii_Interface
+{
+    partial class frmWiiInterface
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.clbWiimote = new System.Windows.Forms.CheckedListBox();
+            this.gbWiimote = new System.Windows.Forms.GroupBox();
+            this.gbNunchuck = new System.Windows.Forms.GroupBox();
+            this.clbNunchuck = new System.Windows.Forms.CheckedListBox();
+            this.cbEnabled = new System.Windows.Forms.CheckBox();
+            this.gbWiimoteAccelerometers = new System.Windows.Forms.GroupBox();
+            this.gbNunchuckAccelerometers = new System.Windows.Forms.GroupBox();
+            this.gbJoysticks = new System.Windows.Forms.GroupBox();
+            this.lblWiimoteAccelerometers = new System.Windows.Forms.Label();
+            this.lblNunchuckAccelerometers = new System.Windows.Forms.Label();
+            this.lblJoystick = new System.Windows.Forms.Label();
+            this.gbDevicePath = new System.Windows.Forms.GroupBox();
+            this.txtDevicePath = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tblblBattery = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tbpbBattery = new System.Windows.Forms.ToolStripProgressBar();
+            this.tblblBatteryPercent = new System.Windows.Forms.ToolStripStatusLabel();
+            this.gbWiimote.SuspendLayout();
+            this.gbNunchuck.SuspendLayout();
+            this.gbWiimoteAccelerometers.SuspendLayout();
+            this.gbNunchuckAccelerometers.SuspendLayout();
+            this.gbJoysticks.SuspendLayout();
+            this.gbDevicePath.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // clbWiimote
+            // 
+            this.clbWiimote.FormattingEnabled = true;
+            this.clbWiimote.Items.AddRange(new object[] {
+            "A",
+            "B",
+            "-",
+            "Home",
+            "+",
+            "1",
+            "2",
+            "Up",
+            "Down",
+            "Left",
+            "Right"});
+            this.clbWiimote.Location = new System.Drawing.Point(6, 19);
+            this.clbWiimote.Name = "clbWiimote";
+            this.clbWiimote.Size = new System.Drawing.Size(88, 169);
+            this.clbWiimote.TabIndex = 0;
+            // 
+            // gbWiimote
+            // 
+            this.gbWiimote.Controls.Add(this.gbWiimoteAccelerometers);
+            this.gbWiimote.Controls.Add(this.clbWiimote);
+            this.gbWiimote.Location = new System.Drawing.Point(14, 27);
+            this.gbWiimote.Name = "gbWiimote";
+            this.gbWiimote.Size = new System.Drawing.Size(225, 194);
+            this.gbWiimote.TabIndex = 1;
+            this.gbWiimote.TabStop = false;
+            this.gbWiimote.Text = "Wiimote";
+            // 
+            // gbNunchuck
+            // 
+            this.gbNunchuck.Controls.Add(this.gbJoysticks);
+            this.gbNunchuck.Controls.Add(this.gbNunchuckAccelerometers);
+            this.gbNunchuck.Controls.Add(this.clbNunchuck);
+            this.gbNunchuck.Controls.Add(this.cbEnabled);
+            this.gbNunchuck.Location = new System.Drawing.Point(14, 227);
+            this.gbNunchuck.Name = "gbNunchuck";
+            this.gbNunchuck.Size = new System.Drawing.Size(225, 153);
+            this.gbNunchuck.TabIndex = 2;
+            this.gbNunchuck.TabStop = false;
+            this.gbNunchuck.Text = "Nunchuck";
+            // 
+            // clbNunchuck
+            // 
+            this.clbNunchuck.FormattingEnabled = true;
+            this.clbNunchuck.Items.AddRange(new object[] {
+            "C",
+            "Z"});
+            this.clbNunchuck.Location = new System.Drawing.Point(6, 42);
+            this.clbNunchuck.Name = "clbNunchuck";
+            this.clbNunchuck.Size = new System.Drawing.Size(88, 34);
+            this.clbNunchuck.TabIndex = 0;
+            // 
+            // cbEnabled
+            // 
+            this.cbEnabled.AutoCheck = false;
+            this.cbEnabled.AutoSize = true;
+            this.cbEnabled.Location = new System.Drawing.Point(6, 19);
+            this.cbEnabled.Name = "cbEnabled";
+            this.cbEnabled.Size = new System.Drawing.Size(65, 17);
+            this.cbEnabled.TabIndex = 3;
+            this.cbEnabled.Text = "Enabled";
+            this.cbEnabled.UseVisualStyleBackColor = true;
+            // 
+            // gbWiimoteAccelerometers
+            // 
+            this.gbWiimoteAccelerometers.Controls.Add(this.lblWiimoteAccelerometers);
+            this.gbWiimoteAccelerometers.Location = new System.Drawing.Point(100, 14);
+            this.gbWiimoteAccelerometers.Name = "gbWiimoteAccelerometers";
+            this.gbWiimoteAccelerometers.Size = new System.Drawing.Size(116, 174);
+            this.gbWiimoteAccelerometers.TabIndex = 1;
+            this.gbWiimoteAccelerometers.TabStop = false;
+            this.gbWiimoteAccelerometers.Text = "Accelerometers";
+            // 
+            // gbNunchuckAccelerometers
+            // 
+            this.gbNunchuckAccelerometers.Controls.Add(this.lblNunchuckAccelerometers);
+            this.gbNunchuckAccelerometers.Location = new System.Drawing.Point(100, 19);
+            this.gbNunchuckAccelerometers.Name = "gbNunchuckAccelerometers";
+            this.gbNunchuckAccelerometers.Size = new System.Drawing.Size(116, 65);
+            this.gbNunchuckAccelerometers.TabIndex = 2;
+            this.gbNunchuckAccelerometers.TabStop = false;
+            this.gbNunchuckAccelerometers.Text = "Accelerometers";
+            // 
+            // gbJoysticks
+            // 
+            this.gbJoysticks.Controls.Add(this.lblJoystick);
+            this.gbJoysticks.Location = new System.Drawing.Point(101, 90);
+            this.gbJoysticks.Name = "gbJoysticks";
+            this.gbJoysticks.Size = new System.Drawing.Size(115, 54);
+            this.gbJoysticks.TabIndex = 4;
+            this.gbJoysticks.TabStop = false;
+            this.gbJoysticks.Text = "Joysticks";
+            // 
+            // lblWiimoteAccelerometers
+            // 
+            this.lblWiimoteAccelerometers.Location = new System.Drawing.Point(6, 16);
+            this.lblWiimoteAccelerometers.Name = "lblWiimoteAccelerometers";
+            this.lblWiimoteAccelerometers.Size = new System.Drawing.Size(100, 155);
+            this.lblWiimoteAccelerometers.TabIndex = 0;
+            this.lblWiimoteAccelerometers.Text = "Accelerometer Data";
+            // 
+            // lblNunchuckAccelerometers
+            // 
+            this.lblNunchuckAccelerometers.Location = new System.Drawing.Point(6, 16);
+            this.lblNunchuckAccelerometers.Name = "lblNunchuckAccelerometers";
+            this.lblNunchuckAccelerometers.Size = new System.Drawing.Size(100, 41);
+            this.lblNunchuckAccelerometers.TabIndex = 0;
+            this.lblNunchuckAccelerometers.Text = "Accelerometer Data";
+            // 
+            // lblJoystick
+            // 
+            this.lblJoystick.Location = new System.Drawing.Point(6, 16);
+            this.lblJoystick.Name = "lblJoystick";
+            this.lblJoystick.Size = new System.Drawing.Size(100, 31);
+            this.lblJoystick.TabIndex = 0;
+            this.lblJoystick.Text = "Joystick Values";
+            // 
+            // gbDevicePath
+            // 
+            this.gbDevicePath.Controls.Add(this.txtDevicePath);
+            this.gbDevicePath.Location = new System.Drawing.Point(14, 386);
+            this.gbDevicePath.Name = "gbDevicePath";
+            this.gbDevicePath.Size = new System.Drawing.Size(225, 47);
+            this.gbDevicePath.TabIndex = 9;
+            this.gbDevicePath.TabStop = false;
+            this.gbDevicePath.Text = "Device Path";
+            // 
+            // txtDevicePath
+            // 
+            this.txtDevicePath.Location = new System.Drawing.Point(6, 19);
+            this.txtDevicePath.Name = "txtDevicePath";
+            this.txtDevicePath.ReadOnly = true;
+            this.txtDevicePath.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.txtDevicePath.Size = new System.Drawing.Size(210, 20);
+            this.txtDevicePath.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(255, 24);
+            this.menuStrip1.TabIndex = 10;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tblblBattery,
+            this.tbpbBattery,
+            this.tblblBatteryPercent});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 448);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(255, 22);
+            this.statusStrip1.TabIndex = 11;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tblblBattery
+            // 
+            this.tblblBattery.Name = "tblblBattery";
+            this.tblblBattery.Size = new System.Drawing.Size(92, 17);
+            this.tblblBattery.Text = "Wiimote Battery";
+            // 
+            // tbpbBattery
+            // 
+            this.tbpbBattery.Name = "tbpbBattery";
+            this.tbpbBattery.Size = new System.Drawing.Size(100, 16);
+            // 
+            // tblblBatteryPercent
+            // 
+            this.tblblBatteryPercent.Name = "tblblBatteryPercent";
+            this.tblblBatteryPercent.Size = new System.Drawing.Size(47, 17);
+            this.tblblBatteryPercent.Text = "Percent";
+            // 
+            // frmWiiInterface
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(255, 470);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.gbDevicePath);
+            this.Controls.Add(this.gbNunchuck);
+            this.Controls.Add(this.gbWiimote);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "frmWiiInterface";
+            this.Text = "Wii Interface";
+            this.Load += new System.EventHandler(this.frmWiiInterface_Load);
+            this.gbWiimote.ResumeLayout(false);
+            this.gbNunchuck.ResumeLayout(false);
+            this.gbNunchuck.PerformLayout();
+            this.gbWiimoteAccelerometers.ResumeLayout(false);
+            this.gbNunchuckAccelerometers.ResumeLayout(false);
+            this.gbJoysticks.ResumeLayout(false);
+            this.gbDevicePath.ResumeLayout(false);
+            this.gbDevicePath.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox clbWiimote;
+        private System.Windows.Forms.GroupBox gbWiimote;
+        private System.Windows.Forms.GroupBox gbNunchuck;
+        private System.Windows.Forms.CheckedListBox clbNunchuck;
+        private System.Windows.Forms.CheckBox cbEnabled;
+        private System.Windows.Forms.GroupBox gbWiimoteAccelerometers;
+        private System.Windows.Forms.GroupBox gbJoysticks;
+        private System.Windows.Forms.GroupBox gbNunchuckAccelerometers;
+        private System.Windows.Forms.Label lblWiimoteAccelerometers;
+        private System.Windows.Forms.Label lblNunchuckAccelerometers;
+        private System.Windows.Forms.Label lblJoystick;
+        private System.Windows.Forms.GroupBox gbDevicePath;
+        private System.Windows.Forms.TextBox txtDevicePath;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel tblblBattery;
+        private System.Windows.Forms.ToolStripProgressBar tbpbBattery;
+        private System.Windows.Forms.ToolStripStatusLabel tblblBatteryPercent;
+    }
+}
+
---
+++ Wii Interface/frmWiiInterface.cs
@@ -0,0 +1,139 @@
+﻿/* Adapted from Brian Peek's code which can be downloaded from http://www.codeplex.com/WiimoteLib
+ * By Nam Tran
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WiimoteLib;
+
+namespace Wii_Interface
+{
+    public partial class frmWiiInterface : Form
+    {
+        // Delegates for updated remote status events
+        private delegate void UpdateWiimoteStateDelegate(WiimoteChangedEventArgs args);
+        private delegate void UpdateExtensionChangedDelegate(WiimoteExtensionChangedEventArgs args);
+
+        // Creates the Wii remote
+        Wiimote wiimote;
+
+        // Initialises the form
+        public frmWiiInterface()
+        {
+            InitializeComponent();
+        }
+
+        // Loads the form as well as connects a Wii remote
+        private void frmWiiInterface_Load(object sender, EventArgs e)
+        {
+            wiimote = new Wiimote();
+
+            // Sets the event for when the state of the Wii remote changes
+            wiimote.WiimoteChanged += wiimote_WiimoteChanged;
+
+            // Sets the event for when an extension to the remote is added or removed
+            wiimote.WiimoteExtensionChanged += wiimote_WiimoteExtensionChanged;
+
+            // Connects the Wii remote to the computer
+            wiimote.Connect();
+
+            // Sets the program to report the IR and Accelerometer data from the remote
+            wiimote.SetReportType(InputReport.IRAccel, true);
+
+            // Sets the player LEDS
+            wiimote.SetLEDs(true, false, false, false);
+        }
+
+        private void wiimote_WiimoteChanged(object sender, WiimoteChangedEventArgs args)
+        {
+            // Attempts to update the Wii remote status, but will catch an exception if the remote is unavailable
+            try
+            {
+                BeginInvoke(new UpdateWiimoteStateDelegate(UpdateWiimoteChanged), args);
+            }
+            catch (System.InvalidOperationException){}
+            
+        }
+
+        private void wiimote_WiimoteExtensionChanged(object sender, WiimoteExtensionChangedEventArgs args)
+        {
+            // Attempts to update the Wii remote status, but will catch an exception if the remote is unavailable
+            try
+            {
+                BeginInvoke(new UpdateExtensionChangedDelegate(UpdateExtensionChanged), args);
+            }
+            catch (System.InvalidOperationException){}
+
+            // If an extension is plugged in, then IR values will be reported from a Wii Motion Plus (assuming there is one)
+            if (args.Inserted)
+            {
+                wiimote.SetReportType(InputReport.IRExtensionAccel, true);
+            }
+            else
+            {
+                wiimote.SetReportType(InputReport.IRAccel, true);
+            }
+        }
+
+        private void UpdateWiimoteChanged(WiimoteChangedEventArgs args)
+        {
+            // Creates object for the Wii remote states
+            WiimoteState wiimoteState = args.WiimoteState;
+
+            // Checks for each Wii remote button and displays their values
+            clbWiimote.SetItemChecked(0, wiimoteState.ButtonState.A);
+            clbWiimote.SetItemChecked(1, wiimoteState.ButtonState.B);
+            clbWiimote.SetItemChecked(2, wiimoteState.ButtonState.Minus);
+            clbWiimote.SetItemChecked(3, wiimoteState.ButtonState.Home);
+            clbWiimote.SetItemChecked(4, wiimoteState.ButtonState.Plus);
+            clbWiimote.SetItemChecked(5, wiimoteState.ButtonState.One);
+            clbWiimote.SetItemChecked(6, wiimoteState.ButtonState.Two);
+            clbWiimote.SetItemChecked(7, wiimoteState.ButtonState.Up);
+            clbWiimote.SetItemChecked(8, wiimoteState.ButtonState.Down);
+            clbWiimote.SetItemChecked(9, wiimoteState.ButtonState.Left);
+            clbWiimote.SetItemChecked(10, wiimoteState.ButtonState.Right);
+
+            // Displays the Wii remote's accelerometer values
+            lblWiimoteAccelerometers.Text = wiimoteState.AccelState.Values.ToString();
+
+            // If there is a nunchuck plugged in, then nunchuck stats will be displayed
+            if (wiimoteState.ExtensionType.Equals(ExtensionType.Nunchuk)) {
+                // Displays nunchuck accelerometer values
+                lblNunchuckAccelerometers.Text = wiimoteState.NunchukState.AccelState.Values.ToString();
+                // Displays nunchuck joystick values
+                lblJoystick.Text = wiimoteState.NunchukState.Joystick.ToString();
+                // Checks for each nunchuck button and displays their values
+                clbNunchuck.SetItemChecked(0, wiimoteState.NunchukState.C);
+                clbNunchuck.SetItemChecked(1, wiimoteState.NunchukState.Z);
+            }
+
+            // Displays the battery level as a progress bar in the status bar
+            tbpbBattery.Value = (wiimoteState.Battery > 0xc8 ? 0xc8 : (int)wiimoteState.Battery);
+            // Displays the battery level as a decimal between 0.0 and 1.0
+            tblblBatteryPercent.Text = wiimoteState.Battery.ToString();
+
+            // Displays the Wii remote's HID path
+            txtDevicePath.Text = wiimote.HIDDevicePath;
+        }
+
+        // Changes the value of the Enabled checkbox in the Nunchuck box
+        private void UpdateExtensionChanged(WiimoteExtensionChangedEventArgs args)
+        {
+            cbEnabled.Checked = args.Inserted;
+        }
+
+        // Disconnects the remote from the program if the form closes
+        public void frmWiiInterface_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            wiimote.Disconnect();
+        }
+    }
+}
